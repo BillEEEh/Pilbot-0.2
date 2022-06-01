fee1cb170655b1dfe831deb689f6ecc94c48f425
Bill Derksen <BillyDerksen@hotmail.com>
2022-06-01 15:20:09 +0200

    update def command

--- commands/def.js
+++ commands/def.js
@@ -5,23 +5,31 @@ module.exports = {
     name: 'def',
     description: 'Grabs a definition for the word from urban dictionary',
     execute(message, args) {
-        const word = message.content.replace(`$${this.name} `, '');;        
+        const word = message.content.replace(`$${this.name} `, '');;
         const options = {
             method: 'GET',
             url: 'https://mashape-community-urban-dictionary.p.rapidapi.com/define',
-            params: {term: word},
+            params: { term: word },
             headers: {
-              'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com',
-              'X-RapidAPI-Key': XRapidAPIKey
+                'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com',
+                'X-RapidAPI-Key': XRapidAPIKey
             }
-          };
-          axios.request(options).then(function (response) {
-              message.reply(`${response.data.list[0].definition} \n \`\`\`${response.data.list[0].example}\`\`\``);
-          }).catch(function (error) {
-              console.error(error);
-              message.reply('Woord niet gevonden in Urban Dictionary.')
-          });
-        
+        };
+        axios.request(options).then(function (response) {
+            const definition = response.data.list[0].definition
+                .replaceAll('[', '')
+                .replaceAll(']', '');
+            const example = response.data.list[0].example
+                .replaceAll('[', '')
+                .replaceAll(']', '');
+
+
+            message.reply(`${definition} \n \`\`\`${example}\`\`\``);
+        }).catch(function (error) {
+            console.error(error);
+            message.reply('Woord niet gevonden in Urban Dictionary.')
+        });
+
         console.log(`${this.name} command uitgevoerd door ${message.author.username}`);
     }
 }

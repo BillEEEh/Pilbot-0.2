836d62b9e681160a4684d2f86471ccbd71ad3f6f
Bill Derksen <BillyDerksen@hotmail.com>
2022-06-06 21:16:48 +0200

    tts toegevoegd en play aangepast

--- commands/tts.js
+++ commands/tts.js
@@ -1,12 +1,25 @@
 const { createAudioPlayer, joinVoiceChannel, NoSubscriberBehavior, createAudioResource, StreamType, AudioPlayerStatus, VoiceConnectionStatus, AudioPlayer } = require('@discordjs/voice');
-const { join } = require('node:path');
-const { createReadStream } = require('node:fs');
+const googleTTS = require('google-tts-api');
 
 module.exports = {
-    name: 'play',
-    description: 'Speelt een soundfile af',
-    async execute(message, args) {
+    name: 'tts',
+    description: 'Speelt tts af',
+    execute(message, args) {
+        //Controleert op permissions
         const voiceChannel = message.member.voice.channel;
+        const permissions = voiceChannel.permissionsFor(message.client.user);
+        if (!permissions.has("SPEAK"))
+            return message.channel.send("You dont have the correct permissions");
+
+        //TTS 
+        const content = message.content.replace(`$${this.name} `, '');
+        const url = googleTTS.getAudioUrl(content, {
+            lang: 'nl',
+            slow: false,
+            host: 'https://translate.google.com',
+        });
+
+        //Maak connection en player aan
         const connection = joinVoiceChannel({
             channelId: voiceChannel.id,
             guildId: voiceChannel.guildId,
@@ -19,20 +32,15 @@ module.exports = {
         });
 
         connection.on(VoiceConnectionStatus.Ready, () => {
-            console.log('The connection has entered the Ready state - ready to play audio!');
+            console.log('The connection has entered the Ready state - ready to play TTS!');
         });
 
         // Controleert op permissions
-        const permissions = voiceChannel.permissionsFor(message.client.user);
-        if (!permissions.has("SPEAK"))
-            return message.channel.send("You dont have the correct permissions");
-
-
-        const resource = createAudioResource(join('./files/', 'test.mp3'));
+        const resource = createAudioResource(url);
 
         connection.subscribe(player);
-
         player.play(resource);
+
 
         player.on(AudioPlayerStatus.Playing, () => {
             console.log('Speler is begonnen met spelen')
@@ -41,6 +49,10 @@ module.exports = {
         player.on(AudioPlayerStatus.Idle, () => {
             connection.destroy();
         })
+
+
+
+
 
         console.log(`${this.name} command uitgevoerd door ${message.author.username}`);
     }
